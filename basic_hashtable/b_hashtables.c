3bf86cda7dc028dfeddc1acf0e1485461396bf2b
kyranmccann <kyran.mccann@gmail.com>
2019-03-27 13:09:49 -0700

    fix bcreate

--- basic_hashtable/b_hashtables.c
+++ basic_hashtable/b_hashtables.c
@@ -71,8 +71,9 @@ unsigned int hash(char *str, int max)
 BasicHashTable *create_hash_table(int capacity)
 {
   BasicHashTable *ht;
+  ht = (BasicHashTable *)malloc(sizeof(BasicHashTable));
   ht -> capacity = capacity;
-  ht -> storage = calloc(capacity, sizeof(Pair *));
+  ht -> storage = (Pair**)calloc(capacity, sizeof(Pair*));
   return ht;
 }
 
